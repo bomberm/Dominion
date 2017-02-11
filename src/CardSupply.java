290608c2eb5bb683a901e6c24c489daae1e5ca93
bomberm <bomberm@oregonstate.edu>
2017-02-10 21:50:48 -0800

    Continuing supply management, now working out passing supply info.

--- src/CardSupply.java
+++ src/CardSupply.java
@@ -1,60 +1,51 @@
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
+import java.util.Random;
 
 public class CardSupply {
-	Stack<Card> copperSupply= 		new Stack<Card>(); //need 46
-	Stack<Card> silverSupply= 		new Stack<Card>(); //need 40
-	Stack<Card> goldSupply= 		new Stack<Card>(); //need 30
-	Stack<Card> estateSupply = 		new Stack<Card>(); //Need 18
-	Stack<Card> duchySupply = 		new Stack<Card>(); //need 12
-	Stack<Card> provinceSupply = 	new Stack<Card>(); //need 12
-	Stack<Card> curseSupply = 		new Stack<Card>(); //need 10 for 2 players
-	Stack<Card> adventurerSupply= 	new Stack<Card>(); //need 10
-	Stack<Card> ambassadorSupply= 	new Stack<Card>(); //need 10
-	Stack<Card> baronSupply= 		new Stack<Card>(); //need 10
-	Stack<Card> councilRoomSupply= 	new Stack<Card>(); //need 10
-	Stack<Card> cutPurseSupply= 	new Stack<Card>(); //need 10
-	Stack<Card> embargoSupply= 		new Stack<Card>(); //need 10
-	Stack<Card> feastSupply= 		new Stack<Card>(); //need 10
-	Stack<Card> gardensSupply= 		new Stack<Card>(); //need 10
-	Stack<Card> greatHallSupply= 	new Stack<Card>(); //need 10
-	Stack<Card> mineSupply= 		new Stack<Card>(); //need 10
-	Stack<Card> smithySupply= 		new Stack<Card>(); //need 10
-	Stack<Card> villageSupply= 		new Stack<Card>(); //need 10
-	Stack<Card> woodcutterSupply= 	new Stack<Card>(); //need 10
-	//List<String> kingdomCards=		new List<String>(); //contain kingdom cards used this game
+	SupplyStack copperSupply, silverSupply, goldSupply, estateSupply, duchySupply, provinceSupply, curseSupply, adventurerSupply;
+	SupplyStack ambassadorSupply, baronSupply, councilRoomSupply, cutPurseSupply, embargoSupply, feastSupply, gardensSupply;
+	SupplyStack greatHallSupply, mineSupply, smithySupply, villageSupply, woodcutterSupply;
+	List<String> kingdomCards=		new LinkedList<String>(); //contain kingdom cards used this game
 	
 	CardSupply(){
-		int i;
+		copperSupply= 		new SupplyStack("copper", 46); //need 46
+		silverSupply= 		new SupplyStack("silver", 40); //need 40
+		goldSupply= 		new SupplyStack("gold", 30);//need 30
+		estateSupply = 		new SupplyStack("estate", 18); //Need 18
+		duchySupply = 		new SupplyStack("duchy", 12); //need 12
+		provinceSupply = 	new SupplyStack("province", 12); //need 12
+		curseSupply = 		new SupplyStack("curse", 10); //need 10 for 2 players
+		adventurerSupply= 	new SupplyStack("adventurer", 10); //need 10
+		ambassadorSupply= 	new SupplyStack("abassador", 10); //need 10
+		baronSupply= 		new SupplyStack("baron", 10); //need 10
+		councilRoomSupply= 	new SupplyStack("council room", 10); //need 10
+		cutPurseSupply= 	new SupplyStack("cutpurse", 10); //need 10
+		embargoSupply= 		new SupplyStack("embargo", 10); //need 10
+		feastSupply= 		new SupplyStack("feast", 10); //need 10
+		gardensSupply= 		new SupplyStack("gardens", 10); //need 10
+		greatHallSupply= 	new SupplyStack("great hall", 10); //need 10
+		mineSupply= 		new SupplyStack("mine", 10); //need 10
+		smithySupply= 		new SupplyStack("smithy", 10); //need 10
+		villageSupply= 		new SupplyStack("village", 10); //need 10
+		woodcutterSupply= 	new SupplyStack("woodcutter", 10); //need 10
 		
-		for(i=0; i<46; i++) copperSupply.push(new Card("copper"));
-		for(i=0; i<40; i++) silverSupply.push(new Card("silver"));
-		for(i=0; i<30; i++) goldSupply.push(new Card("gold"));
-		for(i=0; i<18; i++) estateSupply.push(new Card("estate"));
-		for(i=0; i<12; i++) {
-			duchySupply.push(new Card("duchy"));
-			provinceSupply.push(new Card("province"));
-			}
-		
-		for(i=0; i<10; i++){
-			curseSupply.push(new Card("curse"));
-			adventurerSupply.push(new Card("adventurer"));
-			ambassadorSupply.push(new Card("ambassador"));
-			baronSupply.push(new Card("baron"));
-			councilRoomSupply.push(new Card("council room"));
-			cutPurseSupply.push(new Card("cutpurse"));
-			embargoSupply.push(new Card("embargo"));
-			feastSupply.push(new Card("feast"));
-			gardensSupply.push(new Card("gardens"));
-			greatHallSupply.push(new Card("great hall"));
-			mineSupply.push(new Card("mine"));
-			smithySupply.push(new Card("smithy"));
-			villageSupply.push(new Card("village"));
-			woodcutterSupply.push(new Card("woodcutter"));
-			}
-		}			
-			
+		kingdomCards = selectKingdomCards();
 		}
+
+	private List<String> selectKingdomCards() {
+		String cardList[]={"adventurer", "ambassador", "baron", "council room", "cutpurse", "embargo", "feast", "gardens", "great hall", "mine", "smithy", "village", "woodcutter"};
+		Random randomness=new Random();
+		int index;
+		
+		while(kingdomCards.size()<10){
+			index=randomness.nextInt(13);
+			if(!kingdomCards.contains(cardList[index])) kingdomCards.add(cardList[index]);
+		}
+		
+		return null;
+	}			
+			
 	}
 
 	
---
+++ src/SupplyStack.java
@@ -0,0 +1,14 @@
+
+public class SupplyStack {
+	Card type;
+	int quantity;
+	
+	public SupplyStack(String type, int count){
+		this.type=new Card(type);
+		quantity=count;
+	}
+	
+	public String getType(){
+		return type.cardType;
+	}
+}
