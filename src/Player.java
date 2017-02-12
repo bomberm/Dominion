8134ecccc3fd24e9702d1dcc75ffc89bfa908579
bomberm <bomberm@oregonstate.edu>
2017-02-12 01:25:23 -0800

    Implemented basic card actions, beginnings of buy phase

--- src/Player.java
+++ src/Player.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Player {
 	CardSupply discard, drawPile;
 	List<Card> hand;
-	int deckSize, actions, buys;
+	int deckSize, discardSize, actions, buys;
 	
 	public Player()
 	{
@@ -18,10 +18,57 @@ public class Player {
 			hand.add(drawPile.drawCard());
 		}
 		deckSize=10;
+		discardSize=0;
 	}
 	
 	public List<Card> getHand(){
 		return hand;
+	}
+	
+	public void shuffleDiscard(){ //doesn't actually shuffle, just moves discard back into drawPile
+		drawPile.adventurer.quantity=discard.adventurer.quantity;
+		discard.adventurer.quantity=0;
+		drawPile.ambassador.quantity=discard.ambassador.quantity;
+		discard.ambassador.quantity=0;
+		drawPile.copper.quantity=discard.copper.quantity;
+		discard.copper.quantity=0;
+		drawPile.silver.quantity=discard.silver.quantity;
+		discard.silver.quantity=0;
+		drawPile.gold.quantity=discard.gold.quantity;
+		discard.gold.quantity=0;
+		drawPile.estate.quantity=discard.estate.quantity;
+		discard.estate.quantity=0;
+		drawPile.baron.quantity=discard.baron.quantity;
+		discard.baron.quantity=0;
+		drawPile.duchy.quantity=discard.duchy.quantity;
+		discard.duchy.quantity=0;
+		drawPile.province.quantity=discard.province.quantity;
+		discard.province.quantity=0;
+		drawPile.curse.quantity=discard.curse.quantity;
+		discard.curse.quantity=0;
+		drawPile.councilRoom.quantity=discard.councilRoom.quantity;
+		discard.councilRoom.quantity=0;
+		drawPile.cutPurse.quantity=discard.cutPurse.quantity;
+		discard.cutPurse.quantity=0;
+		drawPile.embargo.quantity=discard.embargo.quantity;
+		discard.embargo.quantity=0;
+		drawPile.feast.quantity=discard.feast.quantity;
+		discard.feast.quantity=0;
+		drawPile.gardens.quantity=discard.gardens.quantity;
+		discard.gardens.quantity=0;
+		drawPile.greatHall.quantity=discard.greatHall.quantity;
+		discard.greatHall.quantity=0;
+		drawPile.mine.quantity=discard.mine.quantity;
+		discard.mine.quantity=0;
+		drawPile.smithy.quantity=discard.smithy.quantity;
+		discard.smithy.quantity=0;
+		drawPile.village.quantity=discard.village.quantity;
+		discard.village.quantity=0;
+		drawPile.woodcutter.quantity=discard.woodcutter.quantity;
+		discard.woodcutter.quantity=0;
+		deckSize=discardSize;
+		discardSize=0;
+		
 	}
 	
 	
--- src/Turn.java
+++ src/Turn.java
@@ -1,16 +1,61 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class Turn {
-	int actions, buys;
+	int actions, buys, deck, discard;
 	List<Card> hand;
 	
-	public Turn(List<Card> hand){
+	public Turn(Player player){
 		actions=1;
 		buys=1;
-		this.hand=new ArrayList<Card>();
-		this.hand=hand;
+		
+		while(actions > 0){
+			playAction(player);
+			actions--;
+		}
+		
+		while(buys > 0){
+			buyCards(player);
+			buys--;
+		}
+		
+		endTurn();
 	}
+
+	private void buyCards(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void endTurn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void playAction(Player player) {
+		for(int i=0; i<player.hand.size(); i++) //go through each card in hand.
+		{
+			if(player.hand.get(i).action){
+				actions+=player.hand.get(i).actionsGranted;
+				buys+=player.hand.get(i).buysGranted;
+				for(int j=0; j<player.hand.get(i).cardsGranted; j++){
+					player.hand.add(player.drawPile.drawCard());
+					player.deckSize--;
+				}
+				if(player.hand.get(i).special) doTheThing(player.hand.get(i));
+				player.discard.addCard(player.hand.get(i).cardType);
+				player.discardSize++;
+				return;
+			}
+		}
+		return;
+	}
+
+	private void doTheThing(Card card) {
+		System.out.println("I do a thing here!");
+		
+	}
+	
+	
 
 	
 }
